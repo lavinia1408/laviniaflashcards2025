74dc8651e6a4bb7b0eb29183011a44f204356eef
lavinia1408 <marcal.lavinia@escola.pr.gov.br>
2025-06-13 11:47:20 +0000

    flashcards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo ?',
+    'Russia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o nome do rio mais longo do mundo?',
+    'Rio Nilo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o nome do maior deserto do mundo?',
+    'Deserto Antártico'
+)
